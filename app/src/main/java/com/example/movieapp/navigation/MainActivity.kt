a1171093551342f89b6abb06aab15b3e1b3a7156
ahataha123 <84542936+ahataha123@users.noreply.github.com>
2023-03-26 23:42:53 +0200

    Refactoring Done LD_03 Done

--- app/src/main/java/com/example/movieapp/navigation/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/navigation/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.movieapp.ui.theme.MovieAppTheme
 
-
+// Done with Adam Ibragimov,Kenan Husic
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
