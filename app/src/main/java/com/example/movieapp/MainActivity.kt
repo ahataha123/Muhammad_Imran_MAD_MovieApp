91c1f322302fe141bca808c8ed2ded4d5a653b0a
ahataha123 <84542936+ahataha123@users.noreply.github.com>
2023-03-26 17:42:52 +0200

    LD_2

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
+import com.example.lectureexamples.R
 import com.example.movieapp.ui.theme.MovieAppTheme
 
 
